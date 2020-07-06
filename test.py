2e0116fedd14b4f8995a4588aecb1f7830aa3086
PsiAmp <kostinkostya@gmail.com>
2020-07-06 14:03:10 +0300

    Debugging logging solution

--- test.py
+++ test.py
@@ -4,15 +4,15 @@ from google.cloud.logging.handlers import CloudLoggingHandler
 
 client = google.cloud.logging.Client()
 handler = CloudLoggingHandler(client)
-cloud_logger = logging.getLogger('cloudLogger')
-cloud_logger.setLevel(logging.INFO) # defaults to WARN
-cloud_logger.addHandler(handler)
-cloud_logger.error('===================+++++++++++++++++  ')
-cloud_logger.error('===================+++++++++++++++++  bad news')
-cloud_logger.error('===================+++++++++++++++++  ')
+log = logging.getLogger('cloudLogger')
+log.setLevel(logging.INFO) # defaults to WARN
+log.addHandler(handler)
+log.error('===================+++++++++++++++++  ')
+log.error('===================+++++++++++++++++  bad news')
+log.error('===================+++++++++++++++++  ')
 
 
 if __name__ == '__main__':
-    logging.info("=-=-=-=-=-=-=-=+++++ INIT =-=-=-=-=-=-=-=+++++")
-    logging.error("=-=-=-=-=-=-=-=+++++ ERROR =-=-=-=-=-=-=-=+++++")
-    logging.info("_Authenticating... v0.9.0\n")
+    log.info("=-=-=-=-=-=-=-=+++++ INIT =-=-=-=-=-=-=-=+++++")
+    log.error("=-=-=-=-=-=-=-=+++++ ERROR =-=-=-=-=-=-=-=+++++")
+    log.info("_Authenticating... v0.9.0\n")
