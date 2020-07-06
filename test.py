4fb2282992cfc5ce151ee416719f7eda55b88104
PsiAmp <kostinkostya@gmail.com>
2020-07-06 13:54:13 +0300

    Debugging logging solution

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import logging
-import google.cloud.logging # Don't conflict with standard logging
-from google.cloud.logging.handlers import CloudLoggingHandler, setup_logging
-
+import google.cloud.logging
+from google.cloud.logging.handlers import CloudLoggingHandler
 
 client = google.cloud.logging.Client()
 handler = CloudLoggingHandler(client)
-logging.getLogger().setLevel(logging.INFO) # defaults to WARN
-setup_logging(handler)
-logging.error('bad news')
+cloud_logger = logging.getLogger('cloudLogger')
+cloud_logger.setLevel(logging.INFO) # defaults to WARN
+cloud_logger.addHandler(handler)
+cloud_logger.error('===================+++++++++++++++++  ')
+cloud_logger.error('===================+++++++++++++++++  bad news')
+cloud_logger.error('===================+++++++++++++++++  ')
+
+
+if __name__ == '__main__':
+    logging.info("=-=-=-=-=-=-=-=+++++ INIT =-=-=-=-=-=-=-=+++++")
+    logging.error("=-=-=-=-=-=-=-=+++++ ERROR =-=-=-=-=-=-=-=+++++")
+    logging.info("_Authenticating... v0.9.0\n")
