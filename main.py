e0691cdc19c745bf94faf434b62bb95d370152fd
PsiAmp <kostinkostya@gmail.com>
2020-07-07 13:31:16 +0300

    Refactored logging init Added -debug launch args

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 import re
 import praw
@@ -11,15 +12,26 @@ import urllib.parse
 from urllib.error import HTTPError, URLError
 from urllib.request import Request
 
-# Init logger that will be visible in Global scope
-client = google.cloud.logging.Client()
-handler = CloudLoggingHandler(client)
-log = logging.getLogger('cloudLogger')
-log.setLevel(logging.INFO)
-log.addHandler(handler)
-
 # Store startup time
 start_time = time.time()
+
+# Is assigned to a platform logger
+log = logging.getLogger('cloudLogger')
+
+
+# Parsing command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-debug', action="store_true", default=False)
+is_debug = parser.parse_args().debug
+
+
+# Init logger that will be visible in Global scope
+def init_logger():
+    log.setLevel(logging.INFO)
+    if not is_debug:
+        client = google.cloud.logging.Client()
+        handler = CloudLoggingHandler(client)
+        log.addHandler(handler)
 
 
 def authenticate():
@@ -63,21 +75,40 @@ def is_link_valid(link):
 def is_comment_summoning(comment):
     body = str(comment.body)
 
+    if is_debug:
+        print(body)
+        print(comment.submission.permalink)
+
     is_recent_comment = comment.created_utc > start_time
-    is_user_me = comment.author == "PsiAmp"
+    is_user_psi = comment.author == "PsiAmp"
+    is_user_me = comment.author == "RECabu"
     rec_matched = re.search("rec", body, re.IGNORECASE)
-    vreddit_matched = re.search("vredditdownloader", body, re.IGNORECASE)
+    recabu_matched = re.search("recabu", body, re.IGNORECASE)
+
+    # vreddit_matched = re.search("vredditdownloader", body, re.IGNORECASE)
 
     # Debug code
-    #id_matched = re.search("0122357063", body, re.IGNORECASE)
-    #if id_matched and is_recent_comment:
+    # id_matched = re.search("0122357063", body, re.IGNORECASE)
+    # if id_matched and is_recent_comment:
     #    return True
 
-    return (rec_matched or vreddit_matched) and len(body) <= 20 and is_recent_comment and is_user_me
+    main_rules = (rec_matched
+                  and len(body) <= 6
+                  and is_recent_comment
+                  and not is_user_me)
+    # and comment.is_root)
+
+    return main_rules
 
 
 def is_video_submission(comment):
     return "v.redd.it" in comment.submission.url
+
+
+def get_video_reply(comment, vid_link):
+    if comment.author == "PsiAmp":
+        return f"[Записал на видеокассету]({vid_link}), мастер PsiAmp."
+    return f"Вжух! [Записал на видеокассету]({vid_link})"
 
 
 def run_bot():
@@ -99,10 +130,10 @@ def run_bot():
                 if is_link_valid(vid_link):
                     log.info(f"Video link from reddittube: {vid_link}")
                     try:
-                        log.info("___Test: Записал")
-                        print("___Test: Записал")
                         # Reply to summoner with a link
-                        comment.reply(f"[Записал на видеокассету]({vid_link}), мастер {comment.author}. id={comment.id}")
+                        comment.reply(get_video_reply(comment, vid_link))
+                        print("Записал")
+                        log.info(f"Replied with a link {vid_link}")
                     except Exception as e:
                         log.info(e)
                 else:
@@ -111,5 +142,6 @@ def run_bot():
 
 if __name__ == '__main__':
     config = load_configuration()
+    init_logger()
     reddit = authenticate()
     run_bot()
